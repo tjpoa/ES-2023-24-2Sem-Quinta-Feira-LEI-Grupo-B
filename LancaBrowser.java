8101ded17d843c0f74e98935d0a332aee3b473a0
tjpoa <tjpoa@iscte-iul.pt>
2024-03-19 23:36:41 +0000

    implementado LancaBrowser
    Lança no browser um ficheiro html com o calendário

---
+++ LancaBrowser.java
@@ -0,0 +1,52 @@
+import java.awt.Desktop;
+import java.awt.event.*;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.File;
+import java.net.URL;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class LancaBrowser {
+3333
+    public static void main(String[] args) throws IOException {
+        String urlStr = "https://github.com/vbasto-iscte/dataset-horarios/raw/main/SalasDeAulaPorTiposDeSala.html";
+        String file = "SalasDeAulaPorTiposDeSala.html";
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(System.getProperty("user.dir") + File.separator + file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+        System.out.println("URL para o ficheiro html remoto a ser descarregado para o sistema de ficheiros local = " + urlStr);
+        System.out.println("Caminho para o ficheiro html local = " + System.getProperty("user.dir"));
+        System.out.println("Nome do ficheiro html local = " + file);
+
+        JFrame frame = new JFrame("A Minha Aplicação");
+        JButton button = new JButton("Mostrar Salas no Browser Web");
+        button.setBounds(20,20,250,50);
+
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                Desktop desk = Desktop.getDesktop();
+                try {
+                    File htmlFile = new File(System.getProperty("user.dir") + File.separator + file);
+                    desk.open(htmlFile);
+                } catch (IOException e1) {e1.printStackTrace();}
+            }
+        });
+        frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
---
+++ out/production/ES-2023-24-2Sem-Quinta-Feira-LEI-Grupo-B/README.md
@@ -0,0 +1,2 @@
+# ES-2023-24-2Sem-Quinta-Feira-LEI-Grupo-B
+Projeto da Unidade Curricular Engenharia de Software
