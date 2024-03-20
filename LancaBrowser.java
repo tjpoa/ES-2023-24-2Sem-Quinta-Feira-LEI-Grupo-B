5b3499bd3d1fb17de5a4cf1ccf8c4b948440e45a
DiogoM7 <pir4ta.extreme@gmail.com>
2024-03-20 21:06:39 +0000

    Implementação da leitura
    Implementação da leitura de ficheiros locais ou ficheiro csv através de um url.

--- LancaBrowser.java
+++ LancaBrowser.java
@@ -10,41 +10,6 @@ public class LancaBrowser {
         JButton localFileButton = new JButton("Choose Local CSV File");
         JButton urlButton = new JButton("Specify URL to CSV File");
 
-<<<<<<< Updated upstream
-    public static void main(String[] args) throws IOException {
-        // Gerar o arquivo HTML a partir do CSV
-        CSVparaHTML.main(args);
-
-        String urlStr = "https://github.com/vbasto-iscte/dataset-horarios/raw/main/SalasDeAulaPorTiposDeSala.html";
-        String file = "SalasDeAulaPorTiposDeSala.html";
-        URL url = new URL(urlStr);
-        BufferedInputStream bis = new BufferedInputStream(url.openStream());
-        FileOutputStream fis = new FileOutputStream(System.getProperty("user.dir") + File.separator + file);
-        byte[] buffer = new byte[1024];
-        int count=0;
-        while((count = bis.read(buffer,0,1024)) != -1)
-        {
-            fis.write(buffer, 0, count);
-        }
-        fis.close();
-        bis.close();
-        System.out.println("URL para o ficheiro html remoto a ser descarregado para o sistema de ficheiros local = " + urlStr);
-        System.out.println("Caminho para o ficheiro html local = " + System.getProperty("user.dir"));
-        System.out.println("Nome do ficheiro html local = " + file);
-
-        JFrame frame = new JFrame("A Minha Aplicação");
-        JButton button = new JButton("Mostrar Salas no Browser Web");
-        button.setBounds(20,20,250,50);
-
-        button.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-
-                try {
-                    File htmlFile = new File("calendario.html");
-                    Desktop.getDesktop().browse(htmlFile.toURI());
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-=======
         localFileButton.setBounds(50, 50, 250, 50);
         urlButton.setBounds(50, 120, 250, 50);
 
@@ -57,7 +22,6 @@ public class LancaBrowser {
                     String csvFilePath = selectedFile.getAbsolutePath();
                     CSVparaHTML.convertCSVtoHTML(csvFilePath);
                     openHTMLFileInBrowser("calendario.html");
->>>>>>> Stashed changes
                 }
             }
         });
